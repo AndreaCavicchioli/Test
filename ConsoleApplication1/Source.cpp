710654626cd627901e5b7da86b62eb3c41ca3b5c
andrea.17.cavicchioli@gmail.com <andrea.17.cavicchioli@gmail.com>
2018-12-09 19:37:01 +0100

    commit master

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	cout << "Ecco i valori della i: " << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << endl;
+	}
+	system("pause");
+}
